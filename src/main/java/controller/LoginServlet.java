08fedd5ae3fa700efd02f1fcbc69916779ff939e
cwan <manwan_444@hotmail.com>
2017-04-11 13:58:05 +0700

    edit

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import function.IdTokenVerifierAndParser;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author C.wan_yo
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        response.setContentType("text/html");
+
+        try {
+
+            String idToken = request.getParameter("id_token");
+            GoogleIdToken.Payload payLoad = IdTokenVerifierAndParser.getPayload(idToken);
+            String name = (String) payLoad.get("name");
+            String email = payLoad.getEmail();
+            String pictureUrl = (String) payLoad.get("picture");
+            System.out.println("User name: " + name);
+            System.out.println("User email: " + email);
+
+            HttpSession session = request.getSession(true);
+            session.setAttribute("userName", name);
+            session.setAttribute("userEmail", email);
+            session.setAttribute("userPicUrl", pictureUrl);
+            request.getServletContext().getRequestDispatcher("/welcome-page.jsp").forward(request, response);
+            
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/java/function/IdTokenVerifierAndParser.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package function;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+/**
+ *
+ * @author C.wan_yo
+ */
+public class IdTokenVerifierAndParser {
+
+    private static final String GOOGLE_CLIENT_ID = "1094634315533-ve4dc0af0jc2gti1opkm7tc60mvgukg9.apps.googleusercontent.com";
+
+    public static GoogleIdToken.Payload getPayload(String tokenString) throws Exception {
+
+        JacksonFactory jacksonFactory = new JacksonFactory();
+        GoogleIdTokenVerifier googleIdTokenVerifier = new GoogleIdTokenVerifier(new NetHttpTransport(), jacksonFactory);
+
+        GoogleIdToken token = GoogleIdToken.parse(jacksonFactory, tokenString);
+
+        if (googleIdTokenVerifier.verify(token)) {
+            GoogleIdToken.Payload payload = token.getPayload();
+            if (!GOOGLE_CLIENT_ID.equals(payload.getAudience())) {
+                throw new IllegalArgumentException("Audience mismatch");
+            } else if (!GOOGLE_CLIENT_ID.equals(payload.getAuthorizedParty())) {
+                throw new IllegalArgumentException("Client ID mismatch");
+            }
+            return payload;
+        } else {
+            throw new IllegalArgumentException("id token cannot be verified");
+        }
+    }
+}
