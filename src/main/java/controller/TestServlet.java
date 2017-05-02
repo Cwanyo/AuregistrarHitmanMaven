d8c7b220117a59930ba0586ebf4e887e2a9ad9bc
cwan <manwan_444@hotmail.com>
2017-05-02 22:28:32 +0700

    add Test

---
+++ src/main/java/controller/TestServlet.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.daoImpl.AuthorityDetailImpl;
+import model.pojo.Authority;
+import model.pojo.FormType;
+import model.pojo.Major;
+import model.pojo.PetitionForm;
+import model.pojo.PetitionFormId;
+import model.pojo.Student;
+import model.pojo.StudentRequest;
+import model.pojo.StudentRequestId;
+import model.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author C.wan_yo
+ */
+public class TestServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        //This use for TESTING ONLY
+        List<StudentRequest> p = null;
+
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery("FROM StudentRequest");
+
+            p = query.list();
+            session.close();
+        } catch (HibernateException e) {
+        }
+        if (p == null) {
+            System.out.println("NULKKKKK+");
+        } else {
+            System.out.println(p.size());
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/main/java/model/dao/AuthorityDetailDao.java
+++ src/main/java/model/dao/AuthorityDetailDao.java
@@ -7,14 +7,26 @@ package model.dao;
 
 import java.util.List;
 import model.pojo.FormType;
+import model.pojo.PetitionForm;
+import model.pojo.StudentRequest;
 
 /**
  *
  * @author SONY
  */
 public interface AuthorityDetailDao {
+
+    //get type of form
+    public List<FormType> getListForm();
+
+    /*
+    get list of student request according to status 
+    w = wait 
+    s = suspend
+     */
+    public List<StudentRequest> getStudentRequests(String status);
     
-    public List<FormType> getListForm();//Pettition form from student
+    public List<PetitionForm> getPetitionFormRequest();
 }
 //Stage
 //
